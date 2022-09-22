1599f5194c035ba764a9d3329899ee54ba84d590
Shreyansh-09 <2509shreyansh@gmail.com>
2022-09-22 20:21:57 +0530

    fix: Bug fixed

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var notes = [2000,500,100,20,10,5,1]
 
 function validateBillandCashAmount(){
     hideMessage()
-    if (billAmount.value > 0){
-        if(cashGiven.value >= billAmount.value){
-            var amountToBeReturned = cashGiven.value - billAmount.value;
+    if (Number(billAmount.value) > 0){
+        if(Number(cashGiven.value) >= Number(billAmount.value)){
+            var amountToBeReturned = Number(cashGiven.value) - Number(billAmount.value);
             calculateNotes(amountToBeReturned)
         }
         else{
