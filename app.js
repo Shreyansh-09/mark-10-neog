e6f3114025cdf3d22bdc973ae35d470d29cd7360
Shreyansh-09 <2509shreyansh@gmail.com>
2022-09-24 13:36:13 +0530

    fix

--- app.js
+++ app.js
@@ -4,26 +4,37 @@ var buttonCheck = document.querySelector("#btn-check")
 var message = document.querySelector("#error-message")
 var noofNotes = document.querySelectorAll(".no-of-notes")
 
+
 var notes = [2000,500,100,20,10,5,1]
 
 
 function validateBillandCashAmount(){
     hideMessage()
-    if (Number(billAmount.value) > 0){
-        if(Number(cashGiven.value) >= Number(billAmount.value)){
-            var amountToBeReturned = Number(cashGiven.value) - Number(billAmount.value);
-            calculateNotes(amountToBeReturned)
-        }
-        else{
-            errorMessage("Do you wanna wash plates?")
-        }
+    if(billAmount.value == "" || cashGiven.value == ""){
+        errorMessage("Please Enter the data.")
+    }
+    else if(Number(cashGiven.value) <= 0){
+        errorMessage("Please enter correct cash amount.")
     }
     else{
-        errorMessage("Invalid Amount")
+        if (Number(billAmount.value) > 0){
+            if(Number(cashGiven.value) >= Number(billAmount.value)){
+                var amountToBeReturned = Number(cashGiven.value) - Number(billAmount.value);
+                calculateNotes(amountToBeReturned)
+            }
+            else{
+                errorMessage("Do you wanna wash plates?")
+            }
+        }
+        else{
+            errorMessage("Invalid Amount")
+        }
     }
+    
 }
 
 function calculateNotes(amount){
+    
     for(var i = 0; i < notes.length; i++){
         var notestobeReturned = Math.trunc(amount/notes[i])
         amount = amount % notes[i]
